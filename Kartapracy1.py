9468a20a7b532db037631e42cf4dfe4b8b9ebc02
1b18MO <114583314+Odziej@users.noreply.github.com>
2022-10-05 12:11:59 +0000

    do zadania 7

--- Kartapracy1.py
+++ Kartapracy1.py
@@ -5,8 +5,57 @@ a = int(input())
 b = int(input())
 
 print(a**2 + b**2)
-'''
+
 
 #zad 2
 
+a = int(input())
+b = int(input())
 
+print((a+b)**2)
+
+#zad 3
+
+a = int(input())
+b = int(input())
+
+print((a-b)**3)
+
+
+#zad 4
+
+a = int(input())
+b = int(input())
+c = int(input())
+
+print(a*b*c)
+
+
+#zad 5
+
+a = int(input())
+b = int(input())
+
+d = (2*(a+b)/5)
+
+e = d*0.4
+
+print(e)
+
+
+#zad 6
+
+brutto = float(input())
+
+Vat = brutto*1.23
+
+print(Vat)
+
+#zad 7 
+
+a = int(input())
+b = int(input())
+
+print(a%b)
+
+'''
